2fe817815d346208ad150eeb95335790f56b6289
Emi2710 <deadourfreedom@gmail.com>
2022-10-15 15:22:27 +0200

    connect server to database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db/index");
 
 //MIDDLEWARES
 app.use(cors());
 app.use(express.json());
+
+//ROUTES
+
 
 app.listen(8000, () => {
     console.log("Server running successfully in port 8000")
